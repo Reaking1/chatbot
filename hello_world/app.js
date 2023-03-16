b7510ccd392cca8bee40b618d2a6d3687c9b4027
Reaking1@ <reabetsweramoswane@gmail.com>
2023-03-16 07:36:46 +0200

    upadated

--- hello_world/app.js
+++ hello_world/app.js
@@ -9,8 +9,40 @@ const port = 8000;
 //Create api
 
 app.post('/hello_world',(req,res) => {
-    res.send('Hello World');
-})
+    res.send(
+        [{
+
+            "message": "Kompose!"
+        
+        }, {
+        
+            "message": "Select the suitable option",
+        
+            "metadata": {
+        
+            "contentType": "300",
+        
+                "templateId": "6",
+        
+                "payload": [{
+        
+                    "title": "Welcome Intent",
+        
+                    "message": "Welcome Intent",
+        
+                }, {
+        
+                    "title": "Fallback Intent",
+        
+                    "message": "Fallback Intent"
+        
+                }]
+        
+            }
+        
+        }])
+        
+        })
 
 app.listen(port,() => {
     console.log('listen port 8000');
